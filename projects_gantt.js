1f840683a57db0edee4fa8685b6252106a6aa4e6
Mollie Peters <mollie_peters@tCLAsol-mwilsons4.local>
2020-11-29 11:41:32 -0700

    comprehensive exam timeline

---
+++ projects_gantt.js
@@ -0,0 +1,35 @@
+gantt
+    title Comprehensive Exam
+    dateFormat  YYYY-MM-DD
+    axisFormat %Y-%b-%d
+    section Brains
+    Version 1                    :active, des1, 2020-11-30, 30d
+    Share v1 with committee      :des2, after des1, 14d
+    First committee meeting      :des3, after des2, 1d
+    Version 2                    :des4, after des3, 52d
+    Share v2 with committee.     :des5, after des4, 7d
+    Second committee meeting     :des6, after des5, 1d
+    Version 3                    :des7, after des6, 52d
+    Share with committee.        :des8, after des7, 7d
+    Comprehensive exam           :des9, 2021-05-14, 1d 
+    section F31    
+    Outline                      :active, c1, 2020-11-30, 30d
+    Version 1                    :c2, after c1, 65d
+    Share with committee         :c3, after c2, 7d
+    Second committee meeting     :c4, after c3, 1d
+    Version 2                    :c5, after c4, 52d
+    Share with committee         :c6, after c5, 7d
+    Comprehensive exam           :c7, 2021-05-14, 1d
+    section XIST
+    Outline         :active, d1, 2020-11-30, 30d
+    Refactor code   :active, d2, 2020-11-30, 30d
+      Version 1     :d3, after d2, 60d
+    section Neuroanatomy
+    Ch. 7           :active, b1, 2020-11-30, 14d
+    Ch. 8           :b2, after b1, 14d 
+    Ch. 9           :b3, after b2, 14d 
+    Ch. 10           :b4, after b3, 14d
+    Ch. 11          :b5, after b4, 14d
+    Ch. 12          :b6, after b5, 14d
+    Review          :b7, after b6, 80d
+    Comprehensive exam :b8, 2021-05-14, 1d
